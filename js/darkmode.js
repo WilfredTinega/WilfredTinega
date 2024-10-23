173eb210c6e4afb38f356c044783729f19ded3ec
lowkieman <lowkieman@gmail.com>
2024-10-23 22:21:20 +0300

    created dark mode css

---
+++ js/darkmode.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".bi-circle-half").click(function(){
+        $("header").toggleClass("header");
+        $("main").toggleClass("main");
+        $("footer").toggleClass("footer");
+    })
+})
