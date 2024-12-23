e9ef90ee81c628ac95c7d35648d64328111f7096
lowkieman <lowkieman@gmail.com>
2024-12-23 12:12:43 +0300

    added more card for todo

--- src/projects/VanillaJS.jsx
+++ src/projects/VanillaJS.jsx
@@ -8,7 +8,7 @@ function VanillaJS() {
         <div className='wrapper one'>
           <div>
             <h3>Text To Speech</h3>
-            <a href="getspeechfromtextinstantly.netlify.app">here</a> 
+            <a href="https://getspeechfromtextinstantly.netlify.app/">here</a> 
           </div>
         </div>
         <div className='projects-stacks'>
@@ -23,7 +23,7 @@ function VanillaJS() {
         <div className="wrapper two">
           <div>
             <h3>Image Search Engine</h3>
-            <a href="quality-image-search-engine.netlify.app/"> here</a>
+            <a href="https://quality-image-search-engine.netlify.app/"> here</a>
           </div>
           
         </div>
@@ -40,7 +40,23 @@ function VanillaJS() {
         <div className="wrapper three">
           <div>
             <h3>Digital Clock</h3>
-            <a href="dijitooclock.netlify.app">here</a>
+            <a href="https://dijitooclock.netlify.app/">here</a>
+          </div>
+          
+        </div>
+        <div className='projects-stacks'>
+          <span>Javascript</span>
+          <span>SASS</span>
+          <span>CSS3</span>
+          <span>HTML5</span>
+        </div>
+      </div>
+
+      <div className='project-box'>
+        <div className="wrapper three">
+          <div>
+            <h3>To Do List App</h3>
+            <a href="https://to-do-li-st-app.netlify.app/">here</a>
           </div>
           
         </div>
