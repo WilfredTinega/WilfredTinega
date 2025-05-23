50205c8ede90664c375e2f4c68c5538dcd1545ad
wilfredtinega <tinegawilfred2@gmail.com>
2025-05-23 14:15:42 +0300

    updated the hero section

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -9,10 +9,25 @@ const Hero = () => {
               <img className='w-full md:h-full md:rounded-xl' src={Assests.wmt} alt="Wilfred Tinega Image" />  
             </div>
             
-            <div className='w-full md:h-1/2 md:bg-white absolute top-90 md:static bg-[#ffffff01] backdrop-blur-xl p-5 md:rounded-r-2xl md:flex md:flex-col md:justify-center '>
+            <div className='w-full md:h-1/2 md:bg-white absolute md:rounded-2xl top-90 md:static bg-[#ffffff01] backdrop-blur-xl p-5 md:rounded-r-2xl md:flex md:flex-col md:justify-center '>
                 <div className='text-green-500 text-3xl md:text-4xl font-bold  xl:text-5xl capitalize'>wilfred tinega</div>
                 <div className='text-blue-500 text-xl md:text-2xl font-semibold capitalize'>front-end Web developer</div>
-                <div className='flex justify-center items-center gap-2 m-4'><a  className='flex justify-center items-center gap-2 text-blue-500 hover:text-green-500 border-2 rounded-full  px-5' title='Wilfred Tinega Front-End Web Developer Resume' href={Assests.frontend}>Resume<i className='bi-download text-green-500 text-2xl'></i></a></div>
+                
+                <div className='grid grid-cols-3 mt-1 md:grid-cols-4 gap-1'>
+                    <div className='bg-green-500 text-white rounded-full flex justify-center items-center p-1 hover:bg-blue-500'>React.js</div>
+                    <div className='bg-green-500 text-white rounded-full flex justify-center items-center p-1 hover:bg-blue-500'>JavaScript</div>
+                    <div className='bg-green-500 text-white rounded-full flex justify-center items-center p-1 hover:bg-blue-500'>HTML5</div>
+                    <div className='bg-green-500 text-white rounded-full flex justify-center items-center p-1 hover:bg-blue-500'>CSS3</div>
+                    <div className='bg-green-500 text-white rounded-full flex justify-center items-center p-1 hover:bg-blue-500'>Tailwindcss</div>
+                    <div className='bg-green-500 text-white rounded-full flex justify-center items-center p-1 hover:bg-blue-500'>JQuery</div>
+                </div>
+
+                <div className='flex justify-center items-center gap-2 m-4'>
+                    <a  className='flex justify-center items-center gap-2 text-blue-500 hover:text-green-500 border-2 rounded-full  px-5' title='Wilfred Tinega Front-End Web Developer Resume' href={Assests.frontend}>
+                        <span>resume</span>
+                        <i className='bi-download text-green-500 text-2xl'></i>
+                    </a>
+                </div>
 
                 <div className='flex justify-center'>
                     <ul className="flex justify-center md:justify-start gap-4 mt-4">
@@ -44,19 +59,6 @@ const Hero = () => {
                     </ul>
                 </div>
             </div>
-        </div>
-
-        <div className='text-gray-100 bg-gray-800 px-20 pt-20'>
-
-            <div> 
-                <h1 className='text-2xl'>Hobbies</h1>
-                <li>🎮 Gaming</li>
-                <li>📚 Reading sci-fi</li>
-                <li>🧗 Hiking</li>
-                <li>🎧 Music production</li>
-                <li>🌍 Learning languages</li>
-            </div>
-
         </div>
     </div>
   )
